404fa58ffb92d4f7f5693d59a6d3a31489a09c49
Vladislava Fridmann <vladislava.fridmann@gmail.com>
2023-05-20 21:59:34 +0300

    Commit

--- src/src_utils.py
+++ src/src_utils.py
@@ -5,6 +5,7 @@ import json
 path_to_file = os.path.abspath('../data/')
 path_to_transaction = os.path.join(path_to_file, 'operations.json')
 
+
 def download_transactions():
 
     with open(path_to_transaction, 'r', encoding='utf8') as file:
@@ -12,3 +13,11 @@ def download_transactions():
 
         return transactions_list
 
+
+def last_five_transactions(transaction_list):
+
+    sort_list = [transac for transac in transaction_list if transac != {} and transac['state'] == 'EXECUTED']
+    sort_list.sort(key=lambda dictionary: dictionary['date'], reverse=True)
+    clean_list = sort_list[0:5]
+
+    return clean_list

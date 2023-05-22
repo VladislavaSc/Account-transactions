d94dba9f3653f3a3a73770f492afda9e2333d0b7
Vladislava Fridmann <vladislava.fridmann@gmail.com>
2023-05-22 09:18:42 +0300

    Commit

--- test/test_transactions.py
+++ test/test_transactions.py
@@ -20,7 +20,7 @@ def fixture_transaction_1():
 
 @pytest.fixture
 def test__repr__(fixture_transaction_1):
-    assert fixture_transaction_1.__repr__() == 'Class Transaction({"date": "2019-06-21T12:34:06.351022",
+    assert fixture_transaction_1.__repr__() == ''Class Transaction({"date": "2019-06-21T12:34:06.351022",
     "operationAmount": {
       "amount": "25762.92",
       "currency": {
@@ -30,12 +30,16 @@ def test__repr__(fixture_transaction_1):
     },
     "description": "Opening a deposit",
     "to": "Account 90817634362091276762"
-  })'
-
+  })''
 
 def test_date(fixture_transaction_1):
     assert fixture_transaction_1.date() == '21.06.2019'
 
+def test_amount(fixture_transaction_1):
+    assert fixture_transaction_1.amount() == '25762.92'
+
 def test_description(fixture_transaction_1):
     assert fixture_transaction_1.description() == 'Opening a deposit'
 
+def test_to_account(fixture_transaction_1):
+    assert fixture_transaction_1.to_account() == 'Account 90817634362091276762'
